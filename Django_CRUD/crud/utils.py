52912bd3643721f5af02ca00d6863b05effefce7
GAN XIAO NAN <34169020+Ganxiaonan@users.noreply.github.com>
2021-01-23 11:35:17 +0800

    Update utils.py
    Completed

--- Django_CRUD/crud/utils.py
+++ Django_CRUD/crud/utils.py
@@ -3,7 +3,7 @@ from .models import ParcelList,DriverList
 
 def decide_compartment(parcel_postcode):
     try:
-
+        # Sort parcel based on postcode, postcode should be in range [min_postcode,max_postcode]
         compartment = DriverList.objects.filter(min_postcode__lte=parcel_postcode).filter(max_postcode__gte=parcel_postcode).values_list('compartment', flat=True)
         destination = DriverList.objects.filter(min_postcode__lte=parcel_postcode).filter(max_postcode__gte=parcel_postcode).values_list('zone', flat=True)
 
